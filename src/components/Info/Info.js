962081c296b3a5938c4a309e63fabd8925595e95
Karolina Okla <karolinaokla95@gmail.com>
2021-02-09 20:28:01 +0100

    Add Routing, FAQ  and Info

---
+++ src/components/Info/Info.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from '../Container/Container';
+import Hero from '../Hero/Hero';
+import { infoContent } from '../../data/dataStore';
+
+const Info = () => (
+  <Container>
+    <Hero titleText={infoContent.title} imageSource={infoContent.imageSource}/>
+    {infoContent.content}
+  </Container>
+);
+
+export default Info;
